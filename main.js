fd20c9bb25df6afc4195f072aa14a18ed965938a
Nikolas Martens <Nikolas.M@rtens.org>
2018-02-26 09:09:02 +0100

    Integrated with web server

---
+++ main.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const karma = require('./src');
+const flatFile = require('./src/persistence/flat-file');
+
+class RepositoryStrategy extends karma.RepositoryStrategy {
+  notifyAccess(unit) {
+    unit.takeSnapshot();
+    // this.repository.unload(unit);
+  }
+}
+
+const domain = new karma.Domain(new flatFile.EventBus('./data'), new flatFile.SnapshotStore('./data'), new RepositoryStrategy())
+
+  .add(new karma.Aggregate('Test')
+
+    .init(function () {
+      this.total = 0;
+    })
+
+    .applying('food', e=>e.payload.to, function (e) {
+      this.total = e.payload.total;
+    })
+
+    .executing('Foo', e=>e.payload.target, function ({payload:{target, count}}) {
+      return [new karma.Event('food', {to: target, total: this.total + count})]
+    }));
+
+
+var app = express();
+app.use(bodyParser.json());
+
+app.post('/:command', (req, res) => {
+  domain.execute(new karma.Command(req.params.command, req.body, Math.round(Math.random() * 1000000)))
+    .then(() => res.send({success: true}))
+    .catch(e => res.send({error: e.message}))
+});
+
+app.listen(3000, () => console.log('Listening on port 3000'));
+
