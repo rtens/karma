beb73d87bc03ef9b160fa242f73a6311a68b02f5
Nikolas Martens <Nikolas.M@rtens.org>
2018-02-26 09:18:54 +0100

    Listen on $PORT

--- main.js
+++ main.js
@@ -36,5 +36,6 @@ app.post('/:command', (req, res) => {
     .catch(e => res.send({error: e.message}))
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
 
