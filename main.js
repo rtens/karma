39724880fa6b9d19a80a1b36bb58d9e02c9ffbb7
Nikolas Martens <Nikolas.M@rtens.org>
2018-03-02 22:13:38 +0100

    Proof of concept for live query with web socket

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const karma = require('./src/karma');
 const flatFile = require('./src/persistence/flat-file');
+const expressWs = require('express-ws');
 
 class RepositoryStrategy extends karma.RepositoryStrategy {
   onAccess(unit, repository) {
@@ -28,8 +29,7 @@ const domain = new karma.Module(
         throw new Error('Too much');
       }
       return [
-        new karma.Event('food', {count}),
-        new karma.Event('bard', {total: this.total + count}),
+        new karma.Event('food', {count, total: this.total + count}),
       ]
     })
 
@@ -52,6 +52,7 @@ const domain = new karma.Module(
     })
 
     .applying('food', function ({count}) {
+      console.log('food', count);
       this.total += count;
     })
 
@@ -62,6 +63,7 @@ const domain = new karma.Module(
 
 var app = express();
 app.use(bodyParser.json());
+expressWs(app);
 
 app.post('/:command', (req, res) => {
   domain.execute(new karma.Command(req.params.command, req.body, Math.round(Math.random() * 1000000)))
@@ -70,9 +72,21 @@ app.post('/:command', (req, res) => {
 });
 
 app.get('/:query', (req, res) => {
-  domain.respondTo(new karma.Query(req.params.query, req.query))
+  domain.respondTo(new karma.Query(req.params.query, req.query.$ ? JSON.parse(req.query.$) : req.query))
     .then(response => res.send({data: response}))
     .catch(e => res.send({error: e.message}))
+});
+
+app.ws('/:query', function (ws, req) {
+  console.log('message');
+  var query = new karma.Query(req.params.query, req.query.$ ? JSON.parse(req.query.$) : req.query);
+  domain.subscribeTo(query, (response) => {
+    console.log('response', response);
+    ws.send(JSON.stringify({data: response}))
+  }).then(subscription => ws.on('close', () => {
+    console.log('closed');
+    subscription.cancel()
+  }));
 });
 
 var port = process.env.PORT || 3000;
