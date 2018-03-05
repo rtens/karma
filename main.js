30f1f31b011507eb62360e89cb2cdc76839b0b0e
Nikolas Martens <Nikolas.M@rtens.org>
2018-03-05 09:39:07 +0100

    Added saga to demo application

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ class RepositoryStrategy extends karma.RepositoryStrategy {
   }
 }
 
-const domain = new karma.Module(
+new karma.Module(
   new flatFile.EventLog('./data'),
   new flatFile.SnapshotStore('./data'),
   new RepositoryStrategy(),
@@ -52,43 +52,51 @@ const domain = new karma.Module(
     })
 
     .applying('food', function ({count}) {
-      console.log('food', count);
       this.total += count;
     })
 
     .respondingTo('Food', $=>$.of, function () {
       return this.total
-    }));
+    }))
 
+  .add(new karma.Saga('Pete')
 
-var app = express();
-app.use(bodyParser.json());
-expressWs(app);
+    .reactingTo('food', ()=>'pete', function (payload) {
+      console.log('########### ' + JSON.stringify(payload));
+    }))
 
-app.post('/:command', (req, res) => {
-  domain.execute(new karma.Command(req.params.command, req.body, Math.round(Math.random() * 1000000)))
-    .then(() => res.send({success: true}))
-    .catch(e => res.send({error: e.message}))
-});
+  .start()
 
-app.get('/:query', (req, res) => {
-  domain.respondTo(new karma.Query(req.params.query, req.query.$ ? JSON.parse(req.query.$) : req.query))
-    .then(response => res.send({data: response}))
-    .catch(e => res.send({error: e.message}))
-});
+  .then(domain => {
 
-app.ws('/:query', function (ws, req) {
-  console.log('message');
-  var query = new karma.Query(req.params.query, req.query.$ ? JSON.parse(req.query.$) : req.query);
-  domain.subscribeTo(query, (response) => {
-    console.log('response', response);
-    ws.send(JSON.stringify({data: response}))
-  }).then(subscription => ws.on('close', () => {
-    console.log('closed');
-    subscription.cancel()
-  }));
-});
+    const app = express();
+    app.use(bodyParser.json());
+    expressWs(app);
 
-var port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Listening on port ${port}`));
+    app.post('/:command', (req, res) => {
+      domain.execute(new karma.Command(req.params.command, req.body, Math.round(Math.random() * 1000000)))
+        .then(() => res.send({success: true}))
+        .catch(e => res.send({error: e.message}))
+    });
+
+    app.get('/:query', (req, res) => {
+      domain.respondTo(new karma.Query(req.params.query, req.query.$ ? JSON.parse(req.query.$) : req.query))
+        .then(response => res.send({data: response}))
+        .catch(e => res.send({error: e.message}))
+    });
+
+    app.ws('/:query', function (ws, req) {
+      console.log('message');
+      const query = new karma.Query(req.params.query, req.query.$ ? JSON.parse(req.query.$) : req.query);
+      domain.subscribeTo(query, (response) => {
+        ws.send(JSON.stringify({data: response}))
+      }).then(subscription => ws.on('close', () => {
+        console.log('closed');
+        subscription.cancel()
+      }));
+    });
+
+    const port = process.env.PORT || 3000;
+    app.listen(port, () => console.log(`Listening on port ${port}`));
+  });
 
